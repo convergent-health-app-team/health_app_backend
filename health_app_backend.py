e2c0aecaae42453df14cc584db8a9140af66102f
Max Dauber <max@dauberfamily.com>
2019-03-30 11:31:44 -0500

    created user/food objects, started html parsing

--- health_app_backend.py
+++ health_app_backend.py
@@ -1,6 +1,80 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'hello world'
+import requests
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+import urllib.request as urllib2
+import datetime
 
+app = Flask(__name__)
+
+#currently only j2 compatible
+@app.route('/get_available_UT', methods=['GET', 'POST'])
+def index():
+	d = datetime.date.today()
+	#url = "http://hf-food.austin.utexas.edu/foodpro/pickMenu2.asp?locationNum=12&locationName=Jester+2nd+Floor+Dining&dtdate=" + \
+	#    str(d.month) + "%2F" + str(d.day) + "%2F" + str(d.year) + \
+	#   "&mealName=Dinner&sName=The+University+of+Texas+at+Austin+%2D+Housing+and+Dining"
+	month = "03"
+	day = "27"
+	year = "2019"
+	url_j2 = "http://hf-food.austin.utexas.edu/foodpro/pickMenu2.asp?locationNum=12&locationName=Jester+2nd+Floor+Dining&dtdate=" + \
+    	month + "%2F" + day + "%2F" + year + \
+    	"&mealName=Dinner&sName=The+University+of+Texas+at+Austin+%2D+Housing+and+Dining"
+	content = urllib2.urlopen(url_j2).read()
+	soup = BeautifulSoup(content)
+	ret = ''
+	for link in soup.find_all('a'):
+		url2 = "http://hf-food.austin.utexas.edu/foodpro/" + link.get('href')
+		r = requests.get(url2)
+		food_soup = BeautifulSoup(r.text)
+		massage_html(food_soup)
+		ret += r.text
+	return ret
+
+#parse usable data from BeautifulSoup object and create food objects
+def massage_html(obj):
+	foodnames = obj.findAll("div", {"class": "labelrecipe"})
+	for food in foodnames:
+		print(food.text)
+	#foods = obj.findAll("div", {"class": "labelrecipe"})
+
+
+
+@app.route('/store_user_preferences', methods=['PUT'])
+def update_preferences():
+	return 'welcome'
+
+@app.route('/new_user', methods=['PUT'])
+def create_user():
+	return 'welcome'
+
+@app.route('/suggest_menu')
+def return_menu():
+	return 'welcome'
+
+@app.route('/')
+def route():
+	return 'welcome'
+
+class food_():
+	def __init__(self, name, tag, image, nutrition_map, allergens, ingredients):
+		self.name = name
+		self.tag = tag
+		self.image = image
+		self.nutrition_map = nutrition_map
+		self.allergens = allergens
+		self.ingredients = ingredients
+
+class user_():
+	def __init__(self, name, id, email, goals, image, height, weight, bmi, university, allergies, preferences, diet_restricitions):
+		self.name = name
+		self.id = id
+		self.email = email
+		self.image = goals
+		self.height = height
+		self.weight = weight
+		self.bmi = bmi
+		self.university = university
+		self.allergies = allergies
+		self.preferences = preferences
+		self.diet_restricitions = diet_restricitions
